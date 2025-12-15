9d734c0afd6b72e8faaf7696b46c9bdae27aff2d
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2025-12-15 13:05:11 +0100

    exercise agenda 2

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,19 +1,16 @@
-import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    static void main(String[] args) {
-        Control control = new Control();
+    public static void main(String[] args) {
+        Control control = new Control(50);
         Scanner sc = new Scanner(System.in);
-        ArrayList<Object[]> listaContacto = new ArrayList<>();
         int opcion;
         do {
             System.out.printf("--MENU AGENDA--\n" +
                     "1. Agregar persona\n" +
-                    "2, Buscar persona\n" +
+                    "2. Buscar persona\n" +
                     "3. Borrar persona\n" +
-                    "4, Listar persona\n" +
+                    "4. Listar persona\n" +
                     "5. Salir\n");
             System.out.println("Elige una opcion:");
             opcion = sc.nextInt();
@@ -21,10 +18,10 @@ public class Main {
                 System.out.println("opcion no disponible.");
             }
             switch (opcion) {
-                case 1 -> control.agregarPersona(listaContacto, sc);
-                case 2 -> control.buscarPersona(listaContacto, sc);
-                case 3 -> {control.eliminarPersona(listaContacto, sc);}
-                case 4 -> control.listarContacto(listaContacto, sc);
+                case 1 -> control.agregarPersona(sc);
+                case 2 -> control.buscarPersona(sc);
+                case 3 -> {control.eliminarPersona(sc);}
+                case 4 -> control.listarContacto();
                 case 5 -> {
                     System.out.println("saliendo...");
                     System.exit(0);}
